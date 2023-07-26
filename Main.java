9a687ae5dc119b13be1137cd263cc4632e05fc44
Pratyush Birole <birole.pratyush@gmail.com>
2023-07-26 21:01:42 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+    int i=0;
+        while(i<=5){
+            
+           System.out.println("AlgoPrep");
+           i=i+1;
+           
+          
+
+        }
+
+
+}
+}
