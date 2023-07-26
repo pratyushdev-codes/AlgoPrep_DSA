a3a16bc8051c65648a37d62bb2451129c4889b8a
Pratyush Birole <birole.pratyush@gmail.com>
2023-07-26 20:59:41 +0530

    Add files via upload

---
+++ ReverseInteger.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ReverseInteger {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        while(n>0){
+             int lastnumber=n%10;
+             System.out.print(lastnumber);
+             n=n/10;
+
+            }
+    }
+}
