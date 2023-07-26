67b89ce56ea20a0318714095620d015ef271bb67
Pratyush Birole <birole.pratyush@gmail.com>
2023-07-27 01:34:58 +0530

    Add files via upload

---
+++ StarPattern.java
@@ -0,0 +1,21 @@
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+
+// Print this pattern
+
+public class StarPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
