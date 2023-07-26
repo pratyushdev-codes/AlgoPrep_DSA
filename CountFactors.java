51e78988c7a4c145a343a31ebd552a7f96c40b88
Pratyush Birole <birole.pratyush@gmail.com>
2023-07-26 21:47:06 +0530

    Add files via upload

---
+++ CountFactors.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class CountFactors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+
+    
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                
+                System.out.println(i);
+                
+                
+                
+                
+            }
+        }
+    }
+    
+}
