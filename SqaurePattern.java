52a61556abf867d9c7beeefdb09b26a3a2f663d3
Pratyush Birole <birole.pratyush@gmail.com>
2023-07-28 20:29:09 +0530

    Add files via upload

--- SqaurePattern.java
+++ SqaurePattern.java
@@ -18,11 +18,10 @@ public class SqaurePattern {
     public static void main(String args[]) throws Exception {
       Scanner sc= new Scanner(System.in);
      int n=sc.nextInt();
-     for(int j=2;j<=n;j++){
-      System.out.println(" ");
-    for(int i=1; i<=n;i++){
+     for(int i=1;i<=n;i++){
+    for(int j=1; j<=n;j++){
       System.out.print("*");
-     }
+     }System.out.println();
     }
     }
   }
