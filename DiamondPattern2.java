7e7d65392009382841da31a94fcee552207f4ed8
Pratyush Birole <birole.pratyush@gmail.com>
2023-07-29 15:04:55 +0530

    Add files via upload

--- DiamondPattern2.java
+++ DiamondPattern2.java
@@ -8,8 +8,9 @@ import java.util.*;
 public class DiamondPattern2 {
  public static void main(String args[]) {
 
-int n= 5;
-        int nst = 3;
+Scanner sc= new Scanner(System.in);
+int n= sc.nextInt();
+        int nst = n/2+1;
         int nsp = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= nst; j++) {
