5d645644ee5090ddeca2b68e3c542d0d0e4fba5c
Pratyush Birole <birole.pratyush@gmail.com>
2023-08-24 23:02:31 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,32 @@
-import java.util.*;
-public class Main{
-  public static void main(String[] args) {
-    System.out.println("Java File for testing purpose");
+
+//prime number optimised
+//factors optimised
+//Sqrt without math.sqrt 
+// multiply-sum
+
+
+
+
+// You need to find the square root of a given number N. If N is not a pe perfect square, then floor(√x) must be returned.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+
     
+    for(int i = 0;i<n; i++){
+        arr [i]=sc.nextInt();
+    }
+    System.out.println(arr[5]);
 }
-}
+        
+    }
+
+    // Syntax: int[] arr = new int[x]; // x=size of array
+    
+
+
+
