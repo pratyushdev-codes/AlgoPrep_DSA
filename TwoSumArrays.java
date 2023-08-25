65e7d40236652e3ce30ab8d945ff9e6f09862d0f
Pratyush Birole <birole.pratyush@gmail.com>
2023-08-25 16:13:33 +0530

    Add files via upload

--- TwoSumArrays.java
+++ TwoSumArrays.java
@@ -1,34 +1,29 @@
-//Two Sum - Given N array elements , check if there exists a pair(i,j) such that arr[i]+arr[j]==k and i!=j. Note i and j are index values , k is given sum
+// Given n length array "arr" and an integer k, Check if there exists a pair(i,j) such that arr[i]+arr[j] == k and i!=j
+
+
 import java.util.*;
+
 public class TwoSumArrays {
-   
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int[]arr = new int[n];
-        
-        for(int i=0;i<n;i++){
-            arr[i] = scn.nextInt();
-        }   
-        `
-        int k = scn.nextInt();
-        
-        System.out.println(twosum(arr,k));
-        
-        
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        System.out.println(twoSum(arr, k));
     }
-    
-    public static boolean twosum(int[] arr, int k){
+
+    public static boolean twoSum(int[] arr, int k) {
         int n = arr.length;
-        for(int i=0;i<n-1;i++){
-            for(int j = i+1;j<n;j++){
-                if(arr[i] + arr[j] == k){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] + arr[j] == k) {
                     return true;
                 }
             }
         }
-        
         return false;
     }
 }
