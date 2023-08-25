6ddb0068ee980ed53d81c3b37d7c1f709559a572
Pratyush Birole <birole.pratyush@gmail.com>
2023-08-25 16:09:07 +0530

    Update CountOfanArray.java

--- CountOfanArray.java
+++ CountOfanArray.java
@@ -3,31 +3,28 @@
 import java.util.*;
 
 public class CountOfanArray {
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int n= sc.nextInt();
-        int [] arr= new int [n];
-       for(int i =1;i<n;i++){
-          arr[i] =sc.nextInt();
-       }
-
-       int ans= countGreater(arr);
-       System.out.println(ans);
-
-    }public static int countGreater(int arr[]){
-        int max= arr[0];
-        for(int i=1; i<arr.length;i++){
-            if(arr[i]>max){
-                max=arr[i];
+      public static void main(String[] args) {
+         Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = scn.nextInt();
+        }
+    
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(arr[i] > max){
+                max = arr[i];
             }
         }
-        int count=0;
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]==max){
+        
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i] == max){
                 count++;
             }
         }
-        return arr.length-count;
+        
+        System.out.println(n - count);
     }
-    
 }
