388a45c9cf1905172d3f22dd770e11b8cac410b1
Pratyush Birole <birole.pratyush@gmail.com>
2023-08-27 11:56:04 +0530

    Add files via upload

--- ReverseString.java
+++ ReverseString.java
@@ -1,38 +1,17 @@
-// 1.Take as input String.
-
-// Write a function that reverses the string and then print that String.
-// Input Format
-
-// String representing S.
-
-// https://www.hackerrank.com/contests/interview-prep-cohort-1/challenges/found-string-print-reverse
 import java.util.*;
-public class ReverseString{
-    public static void main(String[] args) {
-    Scanner scn=new Scanner(System.in);
-        String st=scn.nextLine();
-        
-        char[] ch = st.toCharArray();
-        reverse(ch);
-        for(int i=0;i<ch.length;i++){
-            System.out.print(ch[i]);
-        }
+public class ReverseString {
+    public static void reverseString(char[] s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        sb = sb.reverse();
+        sb.getChars(0, sb.length(), s, 0);
     }
-
-    private static void reverse(char[]ch) {
-        
-       int sp =0;
-       int ep = ch.length - 1;
-        
-       while(sp < ep){
-           char temp = ch[sp];
-           ch[sp] = ch[ep];
-           ch[ep] = temp;
-           sp++;
-           ep--;
-       }
-        
-        
-
+    
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        System.out.println("Original string: " + Arrays.toString(s));
+ 
+        reverseString(s);
+        System.out.println("Reversed string: " + Arrays.toString(s));
     }
 }
