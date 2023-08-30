99038a2e10c033dbdb50b61d6ab07ce706cc5d0f
Pratyush Birole <birole.pratyush@gmail.com>
2023-08-30 11:41:30 +0530

    Update SwapArray.java

--- SwapArray.java
+++ SwapArray.java
@@ -1,31 +1,21 @@
 import java.util.*;
 public class SwapArray{
-      public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        public static void main(String[] args) {
+       Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = scn.nextInt();
         }
+        int idx1 = scn.nextInt();
+        int idx2 = scn.nextInt();
         
-        int sp = sc.nextInt();
-        int ep = sc.nextInt();
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
         
-        SwapArr(arr, sp, ep);
-
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-    }
-
-    public static void SwapArr(int[] arr, int sp, int ep) {
-        while (sp < ep) {
-            int temp = arr[sp];
-            arr[sp] = arr[ep];
-            arr[ep] = temp;
-            sp++;  // Increment sp
-            ep--;  // Decrement ep
+         for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
         }
     }
 }
