7a8bd6c4dbbd2f8c532bf1b7f09ed750e5e41f6d
Pratyush Birole <birole.pratyush@gmail.com>
2023-09-02 11:28:13 +0530

    Add files via upload

--- PrintLinkedList.java
+++ PrintLinkedList.java
@@ -1,13 +1,24 @@
 public class PrintLinkedList {
+        public static void main(String[] args) {
+        // Create a sample linked list
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+        // Call the function to print the linked list
+        printLinkedList(head);
+    }
 
     // Define a Node class for the linked list
     public static class Node {
         int val;
         Node next;
 
-        Node(int val) {
-            this.val = val;
-            this.next = null;
+        Node(int v1) {
+            val=v1;
+            
+
         }
     }
 
@@ -20,14 +31,5 @@ public class PrintLinkedList {
         }
     }
 
-    public static void main(String[] args) {
-        // Create a sample linked list
-        Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
 
-        // Call the function to print the linked list
-        printLinkedList(head);
-    }
 }
