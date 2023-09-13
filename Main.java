9394de6019a853b13b18d3b2f3d644399bbafe77
Pratyush Birole <birole.pratyush@gmail.com>
2023-09-13 20:13:23 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,20 @@
-import java.util.Scanner;
+import ReverseLinkedList.ListNode;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        printPattern(n);
-    }
+public class main{
+   public ListNode reverseList(ListNode head) {
+if(head == null){
+return null;
+}
 
-    public static void printPattern(int n) {
-        int a = 0, b = 1;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print(a + " ");
-                int next = a + b;
-                a = b;
-                b = next;
-            }
-            System.out.println();
-        }
-    }
+if(head.next==null){
+    return head;
+}
+ListNode prev = null;
+ListNode curr = head;
+while(curr!=null){
+    curr.next= prev;
+    prev = curr;
+    curr=nextNode;
+}
+   }
 }
